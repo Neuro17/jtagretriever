1c829703cc2f5593b38b7581def89e2a9fc1a82c
Neuro17 <biagio.chirico.86@gmail.com>
2015-03-15 01:17:50 +0100

    Spring boot

---
+++ src/main/java/app/SpringBootApp.java
@@ -0,0 +1,27 @@
+package app;
+
+import java.util.Arrays;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan
+@EnableAutoConfiguration
+public class SpringBootApp {
+
+    public static void main(String[] args) {
+    	ApplicationContext ctx = SpringApplication.run(SpringBootApp.class, args);
+
+        System.out.println("Let's inspect the beans provided by Spring Boot:");
+
+        String[] beanNames = ctx.getBeanDefinitionNames();
+        Arrays.sort(beanNames);
+        for (String beanName : beanNames) {
+            System.out.println(beanName);
+        }
+    }
+}
